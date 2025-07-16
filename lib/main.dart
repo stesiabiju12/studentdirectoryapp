b9f69cd3e34d9fd6817e3aa04ca4f8bbc6813304
stesiabiju12 <stesia.biju20@gmail.com>
2025-07-16 16:51:32 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MaterialApp(home: ,debugShowCheckedModeBanner: false,));
+}
+
